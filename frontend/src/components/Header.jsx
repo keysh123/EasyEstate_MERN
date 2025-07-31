6e6460f919a8a92c9fc1aea23907d96cbb203107
keysh123 <keyashah126@gmail.com>
2025-07-31 11:41:47 +0530

    search in header interactive

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,27 @@
 import React, { useState } from 'react';
 import { FaSearch, FaBars } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate} from 'react-router-dom';
 import {useSelector} from 'react-redux';
+import { useEffect } from 'react';
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm , setSearchTerm] = useState('')
+  const navigate = useNavigate()
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('searchTerm',searchTerm)
+    const searchQuery = urlParams.toString()
+    navigate(`/search?${searchQuery}`)
+
+
+  }
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search)
+    setSearchTerm(urlParams.get('searchTerm'))
+  },[location.search])
 
   return (
     <header className='bg-slate-200 shadow-md sticky top-0 z-50'>
@@ -19,13 +35,19 @@ const Header = () => {
         </Link>
 
         {/* Search Bar */}
-        <form className='hidden sm:flex bg-slate-100 px-3 py-2 rounded-lg items-center'>
+        <form onSubmit={handleSubmit} className='hidden sm:flex bg-slate-100 px-3 py-2 rounded-lg items-center'>
           <input
             type='text'
             placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64 placeholder-slate-500 text-sm'
+            value={searchTerm}
+            onChange={(e)=>{
+              setSearchTerm(e.target.value)
+            }}
           />
+          <button >
           <FaSearch className='text-slate-500 ml-2' />
+          </button>
         </form>
 
         {/* Desktop Menu */}
