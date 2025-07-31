4f7d77a72bdff5fdbf16e0ea00f22f1e0aa55344
keysh123 <keyashah126@gmail.com>
2025-07-31 11:18:02 +0530

    search api

--- backend/controllers/listingController.js
+++ backend/controllers/listingController.js
@@ -91,16 +91,66 @@ const editListing = async (req, res, next) => {
 };
 const getListing = async (req, res, next) => {
   try {
-    const listing = await Listing.findById(req.params.id).populate('userRef', 'email username');
+    const listing = await Listing.findById(req.params.id).populate(
+      "userRef",
+      "email username"
+    );
     if (!listing) {
       next(errorHandler(404, "No such listing"));
     }
     console.log(listing);
-    
+
     res.status(200).json({
       success: true,
-     
+
       listing,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer
+    if(offer === undefined || offer === false){
+      offer = {$in : [false , true]}
+    }
+
+    let furnished = req.query.furnished
+    if(furnished === undefined || offer === false){
+      furnished = {$in : [false , true]}
+    }
+    let parking = req.query.parking
+    if(parking === undefined || offer === false){
+      parking = {$in : [false , true]}
+    }
+    let type = req.query.type
+    if(type === undefined || type === "all"){
+      type = {$in : ['rent' , 'sale']}
+    }
+    const searchTerm = req.query.searchTerm || '';
+    const sort = req.query.sort || 'createdAt';
+    const order = req.query.order || 'desc'
+
+    const listings = await Listing.find({
+      name : { $regex : searchTerm , $options : 'i'},
+      description : { $regex : searchTerm , $options : 'i'},
+      address : { $regex : searchTerm , $options : 'i'},
+      offer,
+      furnished,
+      parking,
+      type
+    })
+    .sort({
+      [sort] : order
+    })
+    .limit(limit)
+    .skip(startIndex)
+    res.status(200).json({
+      success: true,
+      listings,
     });
   } catch (err) {
     next(err);
@@ -113,4 +163,5 @@ module.exports = {
   deleteListing,
   editListing,
   getListing,
+  getListings,
 };
--- backend/routes/listingRoute.js
+++ backend/routes/listingRoute.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { createListing , getUserListing , deleteListing , editListing , getListing } = require('../controllers/listingController');
+const { createListing , getUserListing , deleteListing , editListing , getListing , getListings } = require('../controllers/listingController');
 const { verifyToken } = require('../utils/verification');
 
 router.post('/create-listing', verifyToken, createListing);
 router.get('/get-listing', verifyToken, getUserListing);
 router.delete('/delete-listing/:id', verifyToken, deleteListing);
 router.put('/edit-listing/:id', verifyToken, editListing);
+router.get('/get',getListings)
 router.get('/:id',getListing)
+
 
 
 
